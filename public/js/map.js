4e1f4a7a5ec4b06a33a515d28d94129caa55d47e
Priya Sharma <priyasharma03456@gmail.com>
2025-05-23 18:41:24 +0530

    added popup

--- public/js/map.js
+++ public/js/map.js
@@ -4,13 +4,13 @@ let map;
 async function initMap() {
   // The location of Uluru
   const position = {
-    lng: coordinates[0],
-    lat: coordinates[1]
+    lat: coordinates[1], 
+    lng: coordinates[0], 
   }; 
   // Request needed libraries.
   //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { Marker } = await google.maps.importLibrary("marker");
 
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
@@ -18,13 +18,38 @@ async function initMap() {
     center: position,
     mapId: 'c74e52ae12fe9797c461afb9'
   });
+
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+
   // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
+  const marker = new Marker({
     map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
     position: position,
+    toggleBounce: toggleBounce,
     title: "Listing Location",
   });
-}
+
+  const infoWindow = new InfoWindow({
+  content: `
+    <div>
+      <strong>Listing Location</strong><br>
+      Location: ${listingLocation}<br>
+      Country: ${listingCountry}
+    </div>`
+  });
+
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker); // this works with classic Marker only
+  });
+  }
 initMap();
 
 
